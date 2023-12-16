2e65d4db15953681502f2b148ef9234eb3714c98
ThiagoScrt <thiagolltt@gmail.com>
2023-12-16 15:21:10 -0300

    Melhora de nome das variaveis

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from uteis import*
-j = escjogo('Deseja calcular de CS2 para Valorant ou de Valorant para CS2?\n[1]: CS2 > VALORANT\n[2] VALORANT > CS2\n: ')
-s = float(input(f'Qual sua sense no {j}? :'))
-if j == 'CS2':
-    print(f'Sua sense no VALORANT é {s/3.18:.3f}')
-elif j == 'VALORANT':
-    print(f'Sua sense no CS2 é {s*3.18:.3f}')
+Jogo = escjogo('Deseja calcular de CS2 para Valorant ou de Valorant para CS2?\n[1]: CS2 > VALORANT\n[2]: VALORANT > CS2\nEscolha: ')
+Sense = float(input(f'Qual sua sense no {Jogo}? :'))
+if Jogo == 'CS2':
+    print(f'Sua sense no VALORANT é : {Sense/3.18:.3f}')
+elif Jogo == 'VALORANT':
+    print(f'Sua sense no CS2 é : {Sense*3.18:.3f}')
--- uteis/__init__.py
+++ uteis/__init__.py
@@ -1,19 +1,20 @@
 def escjogo(msg):
     try:
         while True:
-            n = int(input(msg))
-            if n == 1:
-                jogo = 'CS2'
+            Jogo = int(input(msg))
+            if Jogo == 1:
+                Jogo = 'CS2'
                 break
-            elif n == 2:
-                jogo = 'VALORANT'
+            elif Jogo == 2:
+                Jogo = 'VALORANT'
                 break
             else:
                 print('VALOR INVALIDO')
+                break
     except(ValueError,TypeError):
         print('\033[31mERROR! Por favor digite um valor valido!\033[m')
     except(KeyboardInterrupt):
         print('\033[31mERROR! Nenhum valor digitado')
         return 0
     else:
-        return jogo
+        return Jogo
