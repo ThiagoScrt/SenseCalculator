de7ef707cd244805df15c7eface7e05ebe11d276
ThiagoScrt <thiagolltt@gmail.com>
2023-12-17 22:58:17 -0300

    refatorado

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from uteis import*
+
 Jogo = Esc_Jogo('Deseja calcular de CS2 para Valorant ou de Valorant para CS2?\n[1]: CS2 > VALORANT\n[2]: VALORANT > CS2\nEscolha :')
+
 Sense = Sense_Jogo(f'Qual sua sense? :')
