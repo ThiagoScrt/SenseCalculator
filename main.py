73e904225da2d924a6164832140b29f5965c536c
ThiagoScrt <thiagolltt@gmail.com>
2024-01-25 21:18:10 -0300

    Modificada a forma de tratamendo de erro

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from uteis import*
 
 Jogo = Esc_Jogo('Deseja calcular de CS2 para Valorant ou de Valorant para CS2?\n[1]: CS2 > VALORANT\n[2]: VALORANT > CS2\nEscolha :')
 
-Sense = Sense_Jogo(f'Qual sua sense? :')
+Sensi = Sense_Jogo(f'Qual Sua Sense no {Jogoescolhido["Sense"]}?')
--- uteis/__init__.py
+++ uteis/__init__.py
@@ -15,6 +15,7 @@ def Esc_Jogo(msg):
                 break
     except(ValueError,TypeError):
         print('\033[31mERROR! Por favor digite um valor valido!\033[m')
+
     except(KeyboardInterrupt):
         print('\033[31mERROR! Nenhum valor digitado')
         return 0
@@ -24,8 +25,14 @@ def Esc_Jogo(msg):
 def Sense_Jogo(num):
     Sense = float(input(num))
     if Jogoescolhido['Sense'] == 'CS2':
-        print(f'Sua sense no VALORANT é : {Sense/3.18:.3f}')
+        Sense = Sense/3.18
+        print(f'A conversão direta da sua sense do CS2 para o VALORANT seria : {Sense}')
+        print(f'Para melhor compatibilidade recomendamos usar a sense : {Sense:.2f}')
+
     elif Jogoescolhido['Sense'] == 'VALORANT':
-        print(f'Sua sense no CS2 é : {Sense*3.18:.3f}')
+        Sense = Sense*3.18
+        print(f'A conversão direta da sua sense do VALORANT para o CS2 é : {Sense}')
+        print(f'Para melhor compatibilidade recomendamos usar a sense : {Sense:.2f}')
+
     else:
         print('Alguma coisa deu errado!')
