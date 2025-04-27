713a8b2cd293924b9315027c034f5f55aff75831
Your Name <you@example.com>
2025-04-27 20:37:37 -0300

    REFATORAMENTE DO CODIGO

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from uteis import*
-
-Jogo = Esc_Jogo ('Deseja calcular a sense de qual jogo?\n[1]: CS2\n[2]: VALORANT\n[3]: APEX LEGENDS\n[4]: ARK\nEscolha :')
-jogo = Esc_Jogo ("Para qual jogo deseja converter sua sense ?\n[1]: CS2\n[2]: VALORANT\n[3]: APEX LEGENDS\n[4]: ARK\nEscolha :")
-Sense = Sense_jogo1 (f"Qual sua sense no {Jogoescolhido[0]}? :")
+calculadora = Sensecalculator()
+jogo1 = calculadora.Esc_Jogo("Digite o número do seu jogo atual \n[1]: CS2\n[2]: VALORANT\n[3]: APEX LEGENDS\n[4]: ARK\nEscolha : ")
+jogo2 = calculadora.Esc_Jogo("Digite o número do jogo para o qual você quer converter a sensibilidade \n[1]: CS2\n[2]: VALORANT\n[3]: APEX LEGENDS\n[4]: ARK\nEscolha : ")
+calculadora.Sense_jogo(f"Digite sua sensibilidade no jogo {jogo2}: ")
--- uteis/__init__.py
+++ uteis/__init__.py
@@ -1,50 +1,63 @@
-Jogoescolhido = []
-def Esc_Jogo(num):
-    try:
-        while True:
-            Jogo = int(input(num))
-            if Jogo == 1 or 2 or 3 or 4:
-                Jogo -= 1
-                Jogos = ["CS2", "VALORANT", "APEX", "ARK"] 
-                valor = Jogos[Jogo]
-                break
-            else:
-                print('VALOR INVALIDO')
-                break
-    except(ValueError,TypeError):
-        print('\033[31mERROR! Por favor digite um valor valido!\033[m')
+class Sensecalculator:
+    def __init__(self):
+        self.Jogoescolhido = []
+    def Esc_Jogo(self, num):
+        try:
+            while True:
+                Jogo = int(input(num))
+                if 1 <= Jogo <= 4:
+                    Jogo -= 1
+                    Jogos = ["CS2", "VALORANT", "APEX", "ARK"] 
+                    valor = Jogos[Jogo]
+                    break
+                else:
+                    print('VALOR INVALIDO')
+                    break
+        except(ValueError,TypeError):
+            print('\033[31mERROR! Por favor digite um valor valido!\033[m')
 
-    except(KeyboardInterrupt):
-        print('\033[31mERROR! Nenhum valor digitado')
-        return 0
-    else:
-        Jogoescolhido.append(valor)
+        except(KeyboardInterrupt):
+            print('\033[31mERROR! Nenhum valor digitado')
+            return 0
+        else:
+            self.Jogoescolhido.append(valor)
+        
+    def Sense_jogo(self, num):
+        if not self.Jogoescolhido:
+            print("Nenhum jogo foi escolhido ainda.")
+            return
 
-def Sense_jogo1(num):
-    Sense = float(input(num))
-    while True:
-        if Jogoescolhido[0] == "CS2" "APEX":
-            Sense = Sense
-            break
-        elif Jogoescolhido[0] == "VALORANT":
-            Sense *= 3.1820
-            break
-        elif Jogoescolhido[0] == "ARK":
-            Sense /= 0.1260
-            break
+        try:
+            Sense_inicial = float(input(num))
+        except (ValueError, TypeError):
+            print('\033[31mERROR! Por favor digite um valor valido para a sensibilidade!\033[m')
+            return
+        except KeyboardInterrupt:
+            print('\033[31mOperação interrompida pelo usuário.')
+            return
+
+        Sense = Sense_inicial  # Inicializa Sense com o valor digitado
+
+        if len(self.Jogoescolhido) >= 2:
+            jogo_inicial = self.Jogoescolhido[0]
+            jogo_final = self.Jogoescolhido[1]
+
+            if jogo_inicial == "VALORANT":
+                Sense *= 3.1820
+            elif jogo_inicial == "ARK":
+                Sense /= 0.1260
+            elif jogo_inicial == "APEX" or jogo_inicial == "CS2":
+                Sense = Sense
+
+            if jogo_final == "VALORANT":
+                Sense /= 3.1820
+            elif jogo_final == "ARK":
+                Sense *= 0.1260
+            elif jogo_inicial == "APEX" or jogo_inicial == "CS2":
+                Sense = Sense
+
+            print(f"Sua sense no jogo {jogo_final} É {Sense:.3f}")
+        elif len(self.Jogoescolhido) == 1:
+            print("Você escolheu apenas um jogo. Execute Esc_Jogo novamente para escolher o segundo jogo para a conversão.")
         else:
-            break
-    while True:
-        if Jogoescolhido[1] == "CS2" "APEX":
-            Sense = Sense
-            break
-        elif Jogoescolhido[1] == "VALORANT":
-            Sense /= 3.1820
-            break
-        elif Jogoescolhido[1] == "ARK":
-            Sense *= 0.1260
-            break
-        else:
-            break
-    
-    print(f"Sua sense no jogo {Jogoescolhido[1]} É {Sense:.3f}")
+            print("Nenhum jogo foi escolhido.")
